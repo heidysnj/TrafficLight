b224c3ab5a335444082fb3ac0aa0f0ff2fd704e0
heidysnj <heidysnunez15@gmail.com>
2021-02-23 00:52:04 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,47 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
+	const [state, setState] = React.useState({
+		red: "",
+		yellow: "",
+		green: ""
+	});
+	const stylesBorder = {
+		border: "3px solid yellow",
+		height: "50px",
+		width: "50px"
+	};
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div
+				className="rounded-circle bg-danger"
+				style={
+					state.red ? stylesBorder : { height: "50px", width: "50px" }
+				}
+				onClick={() =>
+					setState({ red: "clicked", yellow: "", green: "" })
+				}></div>
+			<div
+				className="rounded-circle bg-warning"
+				style={
+					state.yellow
+						? stylesBorder
+						: { height: "50px", width: "50px" }
+				}
+				onClick={() =>
+					setState({ red: "", yellow: "clicked", green: "" })
+				}></div>
+			<div
+				className="rounded-circle bg-success"
+				style={
+					state.green
+						? stylesBorder
+						: { height: "50px", width: "50px" }
+				}
+				onClick={() =>
+					setState({ red: "", yellow: "", green: "clicked" })
+				}></div>
 		</div>
 	);
 }
